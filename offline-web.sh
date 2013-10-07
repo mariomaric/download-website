b848356dbf3a41a917ca5b2f397ed4294401fc34
Mario Marić <git@mariomaric.net>
2013-10-07 23:24:11 +0200

    Added script file.

---
+++ offline-web.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Requirements: wget
+# Usage: download-website-offline URL 
+
+wget \
+  --timestamping \
+  --recursive \
+  --adjust-extension \
+  --convert-links \
+  --backup-converted \
+  --page-requisites \
+  "$1"
+
+# Useful extra options:
+# --no-directories
+# --no-check-certificate
+# --accept
+# --reject
+# --no-parent
+# --limit-rate=
+# --quota=
+# --background
+
+exit
